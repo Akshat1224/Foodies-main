af3fa52109001a910b7b1a07756455d99c0b6c48
Akshat Kushwaha <akshatkushwaha7oct2003@gmail.com>
2025-04-16 23:29:43 +0530

    commit the updates

---
+++ server.js
@@ -0,0 +1,83 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3300;
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo'); // Correctly import connect-mongo
+const passport = require('passport');
+const Emitter = require('events');
+
+// Database connection
+const connectDB = require('./app/config/db');
+const uri = process.env.MONGO_CONNECTION_URL;
+connectDB(uri);
+
+// Session store
+let mongoStore = new MongoDbStore({
+    mongoUrl: uri, // Use your MongoDB connection URL
+    collectionName: 'sessions' // Specify the collection name
+});
+
+// Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: mongoStore,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+}));
+
+// Event emitter
+const eventEmitter = new Emitter();
+app.set('eventEmitter', eventEmitter);
+
+// Passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+// Assets
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    res.locals.user = req.user;
+    next();
+});
+
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'ejs');
+
+require('./routes/web')(app);
+
+const server = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+// Socket
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    socket.on('join', (orderId) => {
+        socket.join(orderId);
+    });
+});
+
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data);
+});
+
+eventEmitter.on('orderPlaced', (data) => {
+    io.to('adminRoom').emit('orderPlaced', data);
+});
